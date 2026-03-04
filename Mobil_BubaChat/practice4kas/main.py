436e2cb6a03360f3a31268ad111c8744903a4325
xirokami <kiro.kind@mail.ru>
2026-03-04 19:55:26 +0300

    очистка

--- Mobil_BubaChat/practice4kas/main.py
+++
@@ -1,9 +0,0 @@
-class TodoList:
-    def __init__ (self):
-        self.tasks = []
-    def add_task(self,task):
-        self.tasks.append(task)
-    def remove_task(self,task):
-        self.tasks.remove(task)
-    def get_tasks(self):
-        return self.tasks
--- Mobil_BubaChat/practice4kas/run_tests.py
+++
@@ -1,4 +0,0 @@
-import unittest 
-import tests.test_modules
-if __name__ == '__main__':               
-    unittest.main(module=tests.test_modules)
