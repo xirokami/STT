6aad614aecc142eef5a9a60390ad8e4609f4ddab
xirokami <Kiro.kind@mail.ru>
2026-04-13 13:26:05 +0300

    clear from another branch

--- Mordanov/practice4/main.py
+++
@@ -1,7 +0,0 @@
-class StringManipulator:
-    def reversed_string(self,string):
-        return string[::-1]
-    def to_uppercase(self,string):
-        return string.upper()
-    def to_lowercase(self,string):
-        return string.lower()
