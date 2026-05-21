52b2e8fbf77c4de22702a093102b016014abbab9
xirokami <Kiro.kind@mail.ru>
2026-05-21 17:56:11 +0300

    america

--- Selyakov/python/test/main.py
+++ Selyakov/python/test/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="public"))
+app.mount("/public", StaticFiles(directory="public"), name="public")
+
+
+@app.get("/")
+def root():
+    return FileResponse("index.html")
